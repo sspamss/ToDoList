96b4f0074e4a4930dd42235231fc19a1f73bf9b3
Pam <pamkitsuwan@gmail.com>
2023-03-05 17:52:03 -0500

    Initial commit using Dr. Leinecker's MERN A code

---
+++ frontend/src/components/CardUI.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+function CardUI()
+{
+    var card = '';
+    var search = '';
+
+    const [message,setMessage] = useState('');
+    const [searchResults,setResults] = useState('');
+    const [cardList,setCardList] = useState('');
+    let _ud = localStorage.getItem('user_data');
+    let ud = JSON.parse(_ud);
+    let userId = ud.id;
+    let firstName = ud.firstName;
+    let lastName = ud.lastName;
+	
+    const addCard = async event => 
+    {
+	    event.preventDefault();
+
+        let obj = {userId:userId,card:card.value};
+        let js = JSON.stringify(obj);
+
+        try
+        {
+            const response = await fetch('http://localhost:5000/api/addcard',
+            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            let txt = await response.text();
+            let res = JSON.parse(txt);
+
+            if( res.error.length > 0 )
+            {
+                setMessage( "API Error:" + res.error );
+            }
+            else
+            {
+                setMessage('Card has been added');
+            }
+        }
+        catch(e)
+        {
+            setMessage(e.toString());
+        }
+
+	};
+
+    const searchCard = async event => 
+    {
+        event.preventDefault();
+        		
+        let obj = {userId:userId,search:search.value};
+        let js = JSON.stringify(obj);
+
+        try
+        {
+            const response = await fetch('http://localhost:5000/api/searchcards',
+            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            let txt = await response.text();
+            let res = JSON.parse(txt);
+            let _results = res.results;
+            let resultText = '';
+            for( var i=0; i<_results.length; i++ )
+            {
+                resultText += _results[i];
+                if( i < _results.length - 1 )
+                {
+                    resultText += ', ';
+                }
+            }
+            setResults('Card(s) have been retrieved');
+            setCardList(resultText);
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            setResults(e.toString());
+        }
+    };
+}
+
+export default CardUI;
