da1e069f10b0f07c7ee164ff7326118515803f5f
Pranav Nair <nairpranavj@gmail.com>
2023-03-09 23:20:11 -0500

    Added Login and Register APIS

--- server.js
+++ server.js
@@ -3,222 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-var cardList = 
-[
-  'Roy Campanella',
-  'Paul Molitor',
-  'Tony Gwynn',
-  'Dennis Eckersley',
-  'Reggie Jackson',
-  'Gaylord Perry',
-  'Buck Leonard',
-  'Rollie Fingers',
-  'Charlie Gehringer',
-  'Wade Boggs',
-  'Carl Hubbell',
-  'Dave Winfield',
-  'Jackie Robinson',
-  'Ken Griffey, Jr.',
-  'Al Simmons',
-  'Chuck Klein',
-  'Mel Ott',
-  'Mark McGwire',
-  'Nolan Ryan',
-  'Ralph Kiner',
-  'Yogi Berra',
-  'Goose Goslin',
-  'Greg Maddux',
-  'Frankie Frisch',
-  'Ernie Banks',
-  'Ozzie Smith',
-  'Hank Greenberg',
-  'Kirby Puckett',
-  'Bob Feller',
-  'Dizzy Dean',
-  'Joe Jackson',
-  'Sam Crawford',
-  'Barry Bonds',
-  'Duke Snider',
-  'George Sisler',
-  'Ed Walsh',
-  'Tom Seaver',
-  'Willie Stargell',
-  'Bob Gibson',
-  'Brooks Robinson',
-  'Steve Carlton',
-  'Joe Medwick',
-  'Nap Lajoie',
-  'Cal Ripken, Jr.',
-  'Mike Schmidt',
-  'Eddie Murray',
-  'Tris Speaker',
-  'Al Kaline',
-  'Sandy Koufax',
-  'Willie Keeler',
-  'Pete Rose',
-  'Robin Roberts',
-  'Eddie Collins',
-  'Lefty Gomez',
-  'Lefty Grove',
-  'Carl Yastrzemski',
-  'Frank Robinson',
-  'Juan Marichal',
-  'Warren Spahn',
-  'Pie Traynor',
-  'Roberto Clemente',
-  'Harmon Killebrew',
-  'Satchel Paige',
-  'Eddie Plank',
-  'Josh Gibson',
-  'Oscar Charleston',
-  'Mickey Mantle',
-  'Cool Papa Bell',
-  'Johnny Bench',
-  'Mickey Cochrane',
-  'Jimmie Foxx',
-  'Jim Palmer',
-  'Cy Young',
-  'Eddie Mathews',
-  'Honus Wagner',
-  'Paul Waner',
-  'Grover Alexander',
-  'Rod Carew',
-  'Joe DiMaggio',
-  'Joe Morgan',
-  'Stan Musial',
-  'Bill Terry',
-  'Rogers Hornsby',
-  'Lou Brock',
-  'Ted Williams',
-  'Bill Dickey',
-  'Christy Mathewson',
-  'Willie McCovey',
-  'Lou Gehrig',
-  'George Brett',
-  'Hank Aaron',
-  'Harry Heilmann',
-  'Walter Johnson',
-  'Roger Clemens',
-  'Ty Cobb',
-  'Whitey Ford',
-  'Willie Mays',
-  'Rickey Henderson',
-  'Babe Ruth'
-];
 
+
+const url = 'mongodb+srv://User:Pass@fridge.gdztlsl.mongodb.net/?retryWrites=true&w=majority';
 const MongoClient = require("mongodb").MongoClient;
-const url = 'mongodb+srv://RickLeinecker:COP4331Rocks@cluster0.ehunp00.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(url);
 client.connect(console.log("mongodb connected"));
 
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-	
-  var error = '';
-
-  const { login, password } = req.body;
-
-  const db = client.db("COP4331Cards");
-  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  if (results.length > 0)
-  {
-    id = results[0].UserID;
-    fn = results[0].FirstName;
-    ln = results[0].LastName;
-  }
-
-  var ret = { id:id, firstName:fn, lastName:ln, error:''};
-  res.status(200).json(ret);
-});
-
-
-app.post('/api/addcard', async (req, res, next) =>
-{
-  // incoming: userId, color
-  // outgoing: error
-	
-  const { userId, card } = req.body;
-
-  const newCard = {Card:card,UserId:userId};
-  var error = '';
-
-  try
-  {
-    const db = client.db('COP4331Cards');
-    const result = db.collection('Cards').insertOne(newCard);
-  }
-  catch(e)
-  {
-    error = e.toString();
-  }
-
-  cardList.push( card );
-
-  var ret = { error: error };
-  res.status(200).json(ret);
-});
-
-
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-
-  var error = '';
-
-  const { login, password } = req.body;
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
-  {
-    id = 1;
-    fn = 'Rick';
-    ln = 'Leinecker';
-  }
-  else
-  {
-    error = 'Invalid user name/password';
-  }
-
-  var ret = { id:id, firstName:fn, lastName:ln, error:error};
-  res.status(200).json(ret);
-});
-
-app.post('/api/searchcards', async (req, res, next) => 
-{
-  // incoming: userId, search
-  // outgoing: results[], error
-
-  var error = '';
-
-  const { userId, search } = req.body;
-  var _search = search.toLowerCase().trim();
-  var _ret = [];
-
-  for( var i=0; i<cardList.length; i++ )
-  {
-    var lowerFromList = cardList[i].toLocaleLowerCase();
-    if( lowerFromList.indexOf( _search ) >= 0 )
-    {
-      _ret.push( cardList[i] );
-    }
-  }
-
-  var ret = {results:_ret, error:''};
-  res.status(200).json(ret);
-});
+app.use(cors());
+app.use(bodyParser.json());
 
 app.use((req, res, next) => 
 {
@@ -234,4 +27,94 @@ app.use((req, res, next) =>
   next();
 });
 
-app.listen(5050); // start Node + Express server on port 5050
+app.post('/api/addUser', async (req, res, next) =>
+{
+  // incoming: userId, color
+  // outgoing: error
+	
+  const fname = req.body["firstName"]
+  const lname = req.body["lastName"]
+  const uid = req.body["user"]
+  const pass = req.body["password"]
+
+  const newUser = {firstName:fname,lastName:lname,user:uid,password:pass};
+  var error = '';
+
+  try
+  {
+    const db = client.db("Fridge");
+    const result = db.collection('Users').insertOne(newUser);
+  }
+  catch(e)
+  {
+    error = e.toString();
+  }
+
+  //cardList.push( card );
+
+  var ret = { error: error };
+  res.status(200).json(ret);
+});
+
+
+app.post('/api/login', async (req, res, next) => 
+{
+  // incoming: login, password
+  // outgoing: id, firstName, lastName, error
+	
+ var error = '';
+
+  const user = req.body["Login"];
+  const password = req.body["Password"];
+  
+  const db = client.db("Fridge");
+  const results = await db.collection('Users').find({user:user,password:password}).toArray();
+  
+  var id = -1;
+  var fn = '';
+  var ln = '';
+
+  if( results.length > 0 )
+  {
+    id = results[0].user;
+    fn = results[0].firstName;
+    ln = results[0].lastName;
+  }
+
+  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+  res.status(200).json(ret);
+});
+
+app.post('/api/searchcards', async (req, res, next) => 
+{
+  // incoming: userId, search
+  // outgoing: results[], error
+
+  var error = '';
+
+  const { userId, search } = req.body;
+
+  var _search = search.trim();
+  
+  const db = client.db("COP4331Cards");
+  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
+  
+  var _ret = [];
+  for( var i=0; i<results.length; i++ )
+  {
+    _ret.push( results[i].Card );
+  }
+  
+  var ret = {results:_ret, error:error};
+  res.status(200).json(ret);
+});
+
+
+
+
+
+
+
+
+
+app.listen(5050); // start Node + Express server on port 5000
