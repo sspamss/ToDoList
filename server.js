1caaa526976768f1cc338bc30a37b933819de146
Pam <117599391+sspamss@users.noreply.github.com>
2023-03-13 16:43:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const url = 'thefridgelist.herokuapp.com';
+const url = 'mongodb+srv://User:Pass@fridge.gdztlsl.mongodb.net/?retryWrites=true&w=majority';
 const MongoClient = require("mongodb").MongoClient;
 const client = new MongoClient(url);
 
