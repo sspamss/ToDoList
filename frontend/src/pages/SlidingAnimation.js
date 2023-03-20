02d9e6aebfd33e51be295cac004a4d6f736bd255
Pam <pamkitsuwan@knights.ucf.edu>
2023-03-20 16:39:59 -0400

    Update "SIGN IN" and "SIGN UP" toggle animation

--- frontend/src/pages/SlidingAnimation.js
+++ frontend/src/pages/SlidingAnimation.js
@@ -1,51 +1,17 @@
-import React, { useState } from 'react';
-import LoginPage from './LoginPage';
+// import React, { useState } from 'react';
+// import SlidingAnimationStyling from './SlidingAnimationStyling';
 
-const SlidingAnimation = (props) => {
-  const [isLogin, setIsLogin] = useState(true);
+// const SlidingAnimation = (props) => {
+//   return (
+//     <div>
+//       <SlidingAnimationStyling/>
+//       <div id="toggle">
+//         <button class="toggleSignin">SIGN IN</button>
+//         <button class="toggleSignup">SIGN UP</button>
+//         <button class="toggleCurrent">SIGN IN</button>
+//       </div>
+//     </div>
+//   )
+// };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    props.doSignin(event);
-  };
-
-  const handleToggle = (event) => {
-    setIsLogin(event.target.id === 'login');
-  };
-  
-  return (
-    <div className="sliding-animation">
-      <div className={`form-container ${isLogin ? 'login' : 'signup'}`}>
-        {isLogin ? (
-          <form onSubmit={handleSubmit}>
-            // Login form fields and submit button
-          </form>
-        ) : (
-          <form onSubmit={handleSubmit}>
-            // Signup form fields and submit button
-          </form>
-        )}
-      </div>
-      <div className="toggle-container">
-        <button
-          type="button"
-          id="login"
-          className={`${isLogin ? 'active' : ''}`}
-          onClick={handleToggle}
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          id="signup"
-          className={`${!isLogin ? 'active' : ''}`}
-          onClick={handleToggle}
-        >
-          Signup
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default SlidingAnimation;
+// export default SlidingAnimation;
--- frontend/src/pages/SlidingAnimationStyling.jsx
+++ frontend/src/pages/SlidingAnimationStyling.jsx
@@ -7,20 +7,69 @@ const SlidingAnimationStyling = () => {
         <!-- Login Form -->
         <style>
 
-        .login-form-container {
+        /* Setting and styling the background of the toggle bar */
+        #toggleSlider {
+          background-color: #FFFFFF;
+          border-radius: 12px;
+          display: flex;
+          font-family: 'Fredoka One', sans-serif;
+          justify-content: space-between;
+          margin-top: 2em;
           position: relative;
-          width: 100%;
-          height: 100%;
-          transition: transform 0.6s ease-in-out;
+          transform: translateX(-50%);
+
+          height: 50px;
+          width: 299px;
+          
+          left: 50%;
+          top: 0px;
         }
         
-        .sign-up-mode {
-          transform: translateX(-100%);
+        /* Setting and stying the black (not active) "SIGN IN" text */
+        #toggleSignin {
+          background-color: transparent;
+          border: none;
+          border-radius: 12px;
+          color: #000000;
+          cursor: pointer;
+          font-size: 18pt;
+          margin-left: auto;
+          margin-right: auto;
+          outline: none;
+          font-family: 'Fredoka One', sans-serif;
         }
-        
-        .login-form-container.sign-up-mode {
-          transform: translateX(100%);
-        }             
+
+        /* Setting and styling the black (not active) "SIGN UP" text */
+        #toggleSignup {
+          background-color: transparent;
+          border: none;
+          border-radius: 12px;
+          color: #000000;
+          cursor: pointer;
+          font-family: 'Fredoka One', sans-serif;
+          font-size: 18pt;
+          margin-left: auto;
+          margin-right: auto;
+          outline: none;
+        }
+
+        /* Setting and styling the white (active) "SIGN IN" text */
+        #toggleCurrent {
+          background-color: #9736C5;
+          border: none;
+          border-radius: 12px;
+          color: #FFFFFF;
+          font-family: 'Fredoka One', sans-serif;
+          font-size: 18pt;
+          margin-left: auto;
+          margin-right: auto;
+          position: absolute;
+          outline: none;
+          transition: all 0.2s ease-in-out;
+
+          height: 50px;
+          width: 148px;
+        }
 
         </style>
         <!-- End Login Form -->
