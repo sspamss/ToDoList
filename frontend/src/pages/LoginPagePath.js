a695b406a60d90cb05af127e7d4a3861c7d5b964
Pam <pamkitsuwan@knights.ucf.edu>
2023-03-21 11:51:39 -0400

    Update LoginPagePath.js

--- frontend/src/pages/LoginPagePath.js
+++ frontend/src/pages/LoginPagePath.js
@@ -1,10 +1,11 @@
 const app_name = 'thefridgelist'
 
+// Function to build the path to the backend
 exports.buildPath = 
-    function buildPath(route)
-    {
-        if (process.env.NODE_ENV === 'production')
-            {return 'https://' + app_name +  '.herokuapp.com/' + route;}
-        else
-            {return 'http://localhost:5050/' + route;}
-    }
+function buildPath(route)
+{
+    // If the app is in production, use the herokuapp link
+    if (process.env.NODE_ENV === 'production') {return 'https://' + app_name +  '.herokuapp.com/' + route;}
+    // If the app is in development, use the localhost link
+    else {return 'http://localhost:5050/' + route;}
+}
