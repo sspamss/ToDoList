8637ba5307aa8b309e6525ff1ef773a41b0a6a87
Pam <pamkitsuwan@knights.ucf.edu>
2023-04-11 21:35:32 -0400

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,53 +1,54 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import CreateListModal from '../components/CreateListModal';
+import HomePageStyling from './HomePageStyling';
 import LoggedInName from '../components/LoggedInName';
 import ToDoIcon from '../graphics/ToDoIcon.png';
-import HomePageStyling from './HomePageStyling';
-import { useHistory } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
 const HomePage = () =>
 {
   let bp = require('./LoginPagePath.js');
+
   const history = useHistory();
-
+  const [cardList, setCardList] = useState('');
+  const [isCreateListOpen, setIsCreateListOpen] = useState(false);
+  const [lists, setLists] = useState([]);
   const [message,setMessage] = useState('');
-  const [searchResults,setResults] = useState('');
-  const [cardList,setCardList] = useState('');
-  let _ud = localStorage.getItem('user_data');
-  let ud = JSON.parse(_ud);
-  let userId = ud.id;
-  let firstName = ud.firstName;
-  let lastName = ud.lastName;
-  var card = '';
-  var search = '';
+  const [searchTaskMessage, setSearchTaskMessage] = useState('');
+  const [selectedList, setSelectedList] = useState("");
+  let _ud = localStorage.getItem('user_data'), ud = JSON.parse(_ud);
+  let userId = ud.id, firstName = ud.firstName, lastName = ud.lastName;
+  var card = '', search = '';
 
-  const logout = () =>
+  // Function that removes user session data from local storage then redirects to sign in page
+  const signout = () =>
   {
-    localStorage.removeItem('user_data'); // Removes user session data from localStorage
-    history.replace('/'); // Redirects to login page, going back a page will redirect to login page
+    localStorage.removeItem('user_data');
+    history.replace('/');
   };
 
-  const addTask = async event => 
+  // Function that creates a new object with the selected list name and push it into the lists array
+  const handleCreateList = (listName) => {
+    setLists(prevLists => [...prevLists, {name: listName}]);
+    setSelectedList(listName);
+    setIsCreateListOpen(false);
+  };  
+  
+  {isCreateListOpen && (<CreateListModal isOpen={isCreateListOpen} onRequestClose={() => setIsCreateListOpen(false)} onCreateList={handleCreateList}/>)}
+  
+  const createTask = async event => 
   {
     event.preventDefault();
-
-    let obj = {userId:userId, card:card.value};
-    let js = JSON.stringify(obj);
+    let obj = {userId:userId, card:card.value}, js = JSON.stringify(obj);
 
     try
     {
       const response = await fetch(bp.buildPath("api/addTask"),{method:'POST', body:js, headers:{'Content-Type':'application/json'}});
 
-      let txt = await response.text();
-      let res = JSON.parse(txt);
+      let txt = await response.text(), res = JSON.parse(txt);
 
-      if (res.error.length > 0)
-      {
-        setMessage( "API Error:" + res.error );
-      }
-      else
-      {
-        setMessage('Card has been added');
-      }
+      if (res.error.length > 0) {setMessage("API Error: " + res.error );}
+      else {setMessage("Card has been added");}
     }
     catch(e)
     {
@@ -55,62 +56,71 @@ const HomePage = () =>
     }
   };
 
+
   const searchTask = async event => 
   {
     event.preventDefault();
-        
-    let obj = {userId:userId, search:search.value};
-    let js = JSON.stringify(obj);
+    let obj = {userId:userId, search:search.value}, js = JSON.stringify(obj);
 
     try
     {
-      const response = await fetch(bp.buildPath("api/search"),{method:'POST', body:js, headers:{'Content-Type':'application/json'}});
+      const response = await fetch(bp.buildPath("api/search"), {method:'POST', body:js, headers:{'Content-Type':'application/json'}});
 
-      let txt = await response.text();
-      let res = JSON.parse(txt);
-      let _results = res.results;
-      let resultText = '';
+      let txt = await response.text(), res = JSON.parse(txt);
+      let _results = res.results, resultText = '';
+
       for (var i = 0; i < _results.length; i++ )
       {
         resultText += _results[i];
-        if (i < _results.length - 1)
-        {
-          resultText += ', ';
-        }
+        if (i < _results.length - 1) {resultText += ', ';}
       }
-      setResults('Card(s) have been retrieved');
+      setSearchTaskMessage('Task(s) have been found.');
       setCardList(resultText);
     }
     catch(e)
     {
       alert(e.toString());
-      setResults(e.toString());
+      setSearchTaskMessage(e.toString());
     }
   };
 
   return (
     <div>
       <HomePageStyling/>
-      <div id = "cardUIDiv"><br/>
-      <div class="form-group">
-          <button type="button" id="logoutButton" class="buttons" onClick={logout}>Log Out</button><br/>
+      <div id="homePage">
+        <div class="form-group">
+            <button id="signoutButton" class="buttons" type="button" onClick={signout}>SIGN OUT</button>
         </div>
         <div class="form-group">
           <img id="todolisticon" src={ToDoIcon} alt="To Do List Logo"/>
         </div>
         <div class="form-group">
-          <button type = "button" id = "addTaskButton" class = "buttons" 
-            onClick = {addTask}> Create New Task </button><br/>
-          <span id = "cardAddResult">{message}</span>
+          <input id="searchText" placeholder="SEARCH" type="text" ref={(c) => search = c}/> 
+          <button id="searchTaskButton" class="buttons" type="button" onClick={searchTask}>SEARCH</button>
         </div>
         <div class="form-group">
-          <input type = "text" id = "searchText" placeholder = "SEARCH TASK" ref = {(c) => search = c}/> 
-          <button type = "button" id = "searchTasksButton" class = "buttons" onClick = {searchTask}> Search </button><br/>
+          <button id="createListButton" onClick={() => setIsCreateListOpen(true)}>CREATE LIST</button>
+          <button id="createTaskButton" class="buttons" type="button" onClick={createTask}>CREATE TASK</button>
+          <button id="editTaskButton" class="buttons" type="button">EDIT TASK</button>
         </div>
-        <div class="form-group">
-          <button type = "button" id = "editTasksButton" class = "buttons"> Edit Tasks </button><br/>
+        {isCreateListOpen && (
+          <CreateListModal 
+            isOpen={isCreateListOpen} 
+            onRequestClose={() => setIsCreateListOpen(false)} 
+            onCreateList={handleCreateList}
+            lists={lists}
+          />
+        )}
+        <div className="list-container">
+          {lists.map((list) => (
+            <div key={list.name} className="list">
+              <div className="list-header">{list.name}</div>
+              <div className="list-body">{/* insert list items here */}</div>
+            </div>
+          ))}
         </div>
-        <span id = "cardSearchResult">{searchResults}</span>
+        <span id="errorMessageCreateTask">{message}</span>
+        <span id="searchTaskMessage">{searchTaskMessage}</span>
       </div>
     </div>
   );
