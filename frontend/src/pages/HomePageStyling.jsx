3938df0f51504972af90f6932cd606da80e3dff8
Pam <pamkitsuwan@knights.ucf.edu>
2023-04-11 21:35:30 -0400

    Update HomePageStyling.jsx

--- frontend/src/pages/HomePageStyling.jsx
+++ frontend/src/pages/HomePageStyling.jsx
@@ -7,147 +7,143 @@ const HomePageStyling = () => {
           <!-- Login Form -->
           <style>
   
-            /* Setting the website's background */
+            /* Setting the website background */
             body {
               background-color: #F6F6EF;
               text-align: center;
             }
 
             /* Setting the logout button */
-            #logoutButton {
+            #signoutButton {
               background-color: #9736C5;
               border: none;
-              border-radius: 10px;
-              box-sizing: border-box;
-              height: 35px;
-              width: 80px;
-
+              border-radius: 6px;
               color: #FFFFFF;
-              font-family: 'Comic Sans MS', sans-serif;
+              font-family: 'Fredoka One', sans-serif;
               font-size: 12pt;
-              font-weight: bold;
-              position: center;
+              position: absolute;
               text-align: center;
 
-              display: inline-flex;
-              align-items: center;
-              justify-content: center;
-
-              position: relative;
-              left: -650px;
+              height: 30px;
+              width: 95px;
+              right: 10px;
+              top: 10px;
             }
-
             /* Changing the logout button color when hovered over */
-            #logoutButton:hover {
+            #signoutButton:hover {
               background-color: #AB6DC9;
             }
-  
+            
             /* Setting the To Do List image */
             #todolisticon {
-              margin-bottom: 3em;
-              margin-top: 1em;
-              width: 30%;
+              margin-top: 3em;
+              width: 400px;
               position: center;
-            }
-
-            /*Setting and styling Add Task button*/
-            #addTaskButton {
-              background-color: #9736C5;
-              border: none;
-              border-radius: 10px;
-              box-sizing: border-box;
-              height: 45px;
-              width: 250px;
-
-              color: #FFFFFF;
-              font-family: 'Comic Sans MS', sans-serif;
-              font-size: 18pt;
-              font-weight: bold;
-              position: center;
-              text-align: center;
-            }
-
-            /* Changing the Add Task button color when hovered over */
-            #addTaskButton:hover {
-              background-color: #AB6DC9;
             }
 
             /* Setting and styling the search bar */
-            #searchText{
+            #searchText {
               color: #6C6C6C;
-              border-radius: 10px;
+              border: 3px solid #9736C5;
+              border-radius: 6px;
               box-sizing: border-box;
               font-size: 12pt;
               font-family: 'Inter', sans-serif;
               text-align: center;
-              margin-bottom: 0em;
+              margin-bottom: 0.5em;
+              margin-right: 0.2em;
               position: relative;
-
-              height: 35px;
-              width: 600px;
-              transform: translateY(-10%);
+   
+              height: 30px;
+              width: 370px;
             }
-
             /* Removing the "focus ring" when the search tasks field is clicked */
             #searchText:focus {
               outline: none;
             }
 
             /* Setting and styling the search button */
-            #searchTasksButton {
+            #searchTaskButton {
               background-color: #9736C5;
 
               color: #FFFFFF;
               border: none;
-              border-radius: 10px;
-              box-sizing: border-box;
+              border-radius: 6px;
 
-              font-size: 15pt;
-              font-family: 'Comic Sans MS', sans-serif;
+              font-size: 12pt;
+              font-family: 'Fredoka One', sans-serif;
               text-align: center;
-              font-weight: bold;
 
               margin-bottom: 0.5em;
-              margin-top: 1.5em;
-              height: 35px;
-              width: 100px;
-            }
+              margin-top: 0.5em;
 
+              height: 30px;
+              width: 85px;
+            }
             /* Changing the Search Task button color when hovered over */
-            #searchTasksButton:hover {
+            #searchTaskButton:hover {
               background-color: #AB6DC9;
             }
 
-            /* Setting and styling the edit button */
-            #editTasksButton {
+            /* Setting and styling "CREATE LIST" button*/
+            #createListButton {
               background-color: #9736C5;
-
               color: #FFFFFF;
               border: none;
-              border-radius: 10px;
-              box-sizing: border-box;
-
+              border-radius: 6px;
               font-size: 12pt;
-              font-family: 'Comic Sans MS', sans-serif;
-              text-align: center;
-              font-weight: bold;
+              font-family: 'Fredoka One', sans-serif;
+              margin-right: 0.5em;
 
-              margin-bottom: 0.5em;
-              margin-top: 0em;
-              height: 35px;
-              width: 100px;
-              align: left;
-
-              display: inline-flex;
-              align-items: center;
-              justify-content: center;
-
-              position: relative;
-              left: -310px;
+              height: 30px;
+              width: 120px;
+            }
+            /* Changing the "CREATE LIST" button color when hovered over */
+            #createListButton:hover {
+              background-color: #AB6DC9;
             }
 
+            /* Setting and styling "CREATE TASK" button*/
+            #createTaskButton {
+              background-color: #9736C5;
+              color: #FFFFFF;
+              border: none;
+              border-radius: 6px;
+              font-size: 12pt;
+              font-family: 'Fredoka One', sans-serif;
+              margin-right: 0.5em;
+
+              height: 30px;
+              width: 125px;
+            }
+            /* Changing the Add Task button color when hovered over */
+            #createTaskButton:hover {
+              background-color: #AB6DC9;
+            }
+            /* Styling the error message (if there is one) */
+            #errorMessageCreateTask {
+              font-size: 9pt;
+              position: absolute;
+              margin-top: 0.9em;
+              top: 370px;
+              transform: translateX(-50%);
+              white-space: nowrap;
+            }
+
+            /* Setting and styling the edit button */
+            #editTaskButton {
+              background-color: #9736C5;
+              color: #FFFFFF;
+              border: none;
+              border-radius: 6px;
+              font-size: 12pt;
+              font-family: 'Fredoka One', sans-serif;
+
+              height: 30px;
+              width: 105px;
+            }
             /* Changing the Edit Tasks button color when hovered over */
-            #editTasksButton:hover {
+            #editTaskButton:hover {
               background-color: #AB6DC9;
             }
   
