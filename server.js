79d3aa9b011be7f15d943ef9e485390025291002
KyleFranklin <kylefranklin554@gmail.com>
2023-04-15 16:36:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const path = require('path');         
-//const { stringify } = require('querystring');
-//const sendEmail = require("./utils/sendEmail");
-///const dotenv = require("dotenv").config();
+const { stringify } = require('querystring');
+const sendEmail = require("./utils/sendEmail");
+const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5050; 
 const app = express();
 
