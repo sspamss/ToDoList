66924e45dfad9989d06142592ce0d634cec2d106
Emmanuelle Rebosura <emm.reb@Knights.ucf.edu>
2023-04-17 17:52:48 -0400

    Create CreateTaskModal.js
    only like 5 lines bc im still figuring this out lol

---
+++ frontend/src/components/CreateTaskModal.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+// import CreateListModalStyling from "./CreateListModalStyling";
+import Modal from "react-modal";
+
+const CreateTaskModal = ({ isOpen, onRequestClose, onCreateTask }) => {
+    // idk what im doing.......
+}
