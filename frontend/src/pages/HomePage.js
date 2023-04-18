8f1814817304c9e5ec8337fb5e1899977426f5d0
Pam <pamkitsuwan@knights.ucf.edu>
2023-04-18 15:23:44 -0400

    Update HomePage.js: create pop up for creating task and combine error messages

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import CreateTaskModal from '../components/CreateTaskModal';
 import DisplayListModal from '../components/DisplayListModal';
 import HomePageStyling from './HomePageStyling';
 import ToDoIcon from '../graphics/ToDoIcon.png';
@@ -12,10 +13,12 @@ const HomePage = () =>
   const history = useHistory();
   const [cardList, setCardList] = useState('');
   const [isCreateListOpen, setIsCreateListOpen] = useState(false);
+  const [isCreateTaskOpen, setIsCreateTaskOpen] = useState(false);
   const [lists, setLists] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const [message,setMessage] = useState('');
-  const [searchTaskMessage, setSearchTaskMessage] = useState('');
   const [selectedList, setSelectedList] = useState("");
+  const [selectedTask, setSelectedTask] = useState("");
 
   let _ud = localStorage.getItem('user_data'), ud = JSON.parse(_ud), userId = ud.id;
   var newtaskContent = "", newtaskTime = "", newtaskCategory = ""; 
@@ -36,31 +39,14 @@ const HomePage = () =>
     setLists(prevLists => [...prevLists, {name: listName}]);
     setSelectedList(listName);
     setIsCreateListOpen(false);
-  };  
-    
-  const createTask = async event => 
-  {
-    event.preventDefault();
-    // Make sure when choosing the task that you have the user provide newtaskContent, newtaskTime (must be in datetime format but as a string), newtaskCategory but make sure its a dropdown of three options.
-    let obj = {taskContent: newtaskContent, time: newtaskTime, category: newtaskCategory, user:user.user}, js = JSON.stringify(obj);
-    if(newtaskContent != '' && newtaskTime != "" && newtaskCategory != ""){
-      try
-      {
-        const response = await fetch(bp.buildPath("api/addTask"),{method:'POST', body:js, headers:{'Content-Type':'application/json'}});
-        let txt = await response.text(), res = JSON.parse(txt);
-
-        if (res.error.length > 0) {setMessage("API Error: " + res.error);}
-        else {setMessage("Task has been added");}
-      }
-      catch(e)
-      {
-        setMessage(e.toString());
-      }
-    }
-    else{
-      setMessage("Unable to add task (not all fields filled in)")
-    }
   };
+
+  // Function that creates a new object with the selected task name and push it into the tasks array
+  const handleCreateTask = (taskName) => {
+    setTasks(prevTasks => [...prevTasks, {name: taskName}]);
+    setSelectedTask(taskName);
+    setIsCreateTaskOpen(false);
+  }; 
 
   // Function that handles the user queries
   const searchTask = async event => 
@@ -71,7 +57,8 @@ const HomePage = () =>
     // Check for any empty fields
     if (obj.search === "")
     {
-      setSearchTaskMessage("* Please type at least one character *");
+      setMessage("");
+      setMessage("* Please type at least one character *");
       setCardList("");
       return;
     }
@@ -91,13 +78,15 @@ const HomePage = () =>
         resultText += _results[i];
         if (i < _results.length - 1) {resultText += ', ';}
       }
-      setSearchTaskMessage("* Task(s) have been found *");
+      setMessage(""); 
+      setMessage("* Task(s) have been found *");
       setCardList(resultText);
     }
     catch(e)
     {
+      setMessage(""); 
       alert(e.toString());
-      setSearchTaskMessage(e.toString());
+      setMessage(e.toString());
     }
   };
 
@@ -117,8 +106,8 @@ const HomePage = () =>
           <button id="searchTaskButton" class="buttons" type="button" onClick={searchTask}>SEARCH</button>
         </div>
         <div class="form-group">
-          <button id="createListButton" onClick={() => setIsCreateListOpen(true)}>CREATE LIST</button>
-          <button id="createTaskButton" class="buttons" type="button" onClick={createTask}>CREATE TASK</button>
+          <button id="createListButton" onClick={() => setIsCreateListOpen(true)}>DISPLAY LIST</button>
+          <button id="createTaskButton" onClick={() => setIsCreateTaskOpen(true)}>CREATE TASK</button>
           <button id="editTaskButton" class="buttons" type="button">EDIT TASK</button>
         </div>
         {isCreateListOpen && (
@@ -129,6 +118,14 @@ const HomePage = () =>
             lists={lists}
           />
         )}
+        {isCreateTaskOpen && (
+          <CreateTaskModal 
+            isOpen={isCreateTaskOpen} 
+            onRequestClose={() => setIsCreateTaskOpen(false)} 
+            onCreateTask={handleCreateTask}
+            tasks={tasks}
+          />
+        )}
         <div className="list-container">
           {lists.map((list) => (
             <div key={list.name} className="list">
@@ -137,8 +134,15 @@ const HomePage = () =>
             </div>
           ))}
         </div>
-        <span id="errorMessageCreateTask">{message}</span>
-        <span id="searchTaskMessage">{searchTaskMessage}</span>
+        <div className="list-container">
+          {tasks.map((task) => (
+            <div key={task.name} className="task">
+              <div className="task-header">{task.name}</div>
+              <div className="task-body">{/* insert list items here */}</div>
+            </div>
+          ))}
+        </div>
+        <span id="errorMessage">{message}</span>
       </div>
     </div>
   );
