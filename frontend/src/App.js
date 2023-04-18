3b30d0a0113fe82199cc2cfda22d364c992908a2
KyleFranklin <kylefranklin554@gmail.com>
2023-04-18 14:05:25 -0400

    added a new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import React from 'react';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import HomePage from './pages/HomePage';
+import emailVerificationPage from './pages/emailVerificationPage';
 import ToggleSlider from './pages/ToggleSlider';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Helmet} from 'react-helmet';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 function App() {
   return (
     <Router>
@@ -16,6 +16,7 @@ function App() {
         <Route exact path="/" component={ToggleSlider}/>
         <Route exact path="/forgot-password" component={ForgotPasswordPage}/>
         <Route exact path="/home" component={HomePage}/>
+        <Route exact path="/verify:token" component={emailVerificationPage}/>
       </Switch>
     </Router>
   );
