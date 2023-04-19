1067d6d4563afa3068633af09b71023895b8cf36
KyleFranklin <kylefranklin554@gmail.com>
2023-04-19 09:46:25 -0400

    Update emailVerificationPage.js

--- frontend/src/pages/emailVerificationPage.js
+++ frontend/src/pages/emailVerificationPage.js
@@ -33,19 +33,22 @@ const emailVerificationPage = () =>
       const response = await fetch(bp.buildPath("api/login"),{method:'POST', body:js, headers:{'Content-Type':'application/json'}});
       var res = JSON.parse(await response.text());
       
+      
       // If sign in is invalid, display an error message
       if (res._id <= 0)
       {
         setMessageSignin("* Username or password is incorrect *");
       }
+      /*
       if(res.verified==false){
         setMessageSignin("* this account has yet to be verified resending email now *");
         await fetch(bp.buildPath("api/emailVerification"),{method:'POST', body:js, headers:{'Content-Type':'application/json'}});
       }
+      */
       // If sign in is valid, store the user's information in local storage and redirect to the home page
       else
       {
-        var user = {firstName:res.firstName, lastName:res.lastName, id:res._id}
+        var user = {firstName:res.firstName, lastName:res.lastName, user:signinUsername.value}
         localStorage.setItem('user_data', JSON.stringify(user));
 
         // Clear the error message
