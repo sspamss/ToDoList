4e5299f9dae543da23aff46917ffec069d2a1e9a
KyleFranklin <kylefranklin554@gmail.com>
2023-04-19 09:41:51 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import HomePage from './pages/HomePage';
+import emailVerificationPage from './pages/emailVerificationPage';
 import ToggleSlider from './pages/ToggleSlider';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" component={ToggleSlider}/>
         <Route exact path="/forgot-password" component={ForgotPasswordPage}/>
         <Route exact path="/home" component={HomePage}/>
+        <Route exact path="/verify" component={emailVerificationPage}/>
       </Switch>
     </Router>
   );
